12d8cdb5409a64cf9f89c3e07bc1959abb776b02
Greg Jordan-Detamore <greg@gregjd.com>
2016-11-14 13:45:52 -0500

    Delete old code

--- _scripts/generate_state_pages.py
+++ _scripts/generate_state_pages.py
@@ -1,14 +1,6 @@
-import os
 import pandas
 
 states = pandas.read_csv('../_data/states.csv')
-
-# for state in states.to_dict('records'):
-#     if not os.path.exists(state['state_code']):
-#         os.makedirs(state['state_code'])
-#     newfile = open('{0}/index.md'.format(state['state_code']), 'w')
-#     newfile.write('--\nlayout: state\nstate_code: {0}\nstate_name: {1}\n--\n'
-#       .format(state['state_code'], state['state_name']))
 
 for state in states.to_dict('records'):
     newfile = open('../_states/' + state['state_code'] + '.md', 'w')
