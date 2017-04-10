6d586d61a82b655e1d68e7416dea366d33c62430
Greg Jordan-Detamore <greg@gregjd.com>
2017-04-10 18:22:08 -0400

    Fix file operations?

--- _scripts/import_wwc_sunlight_status.py
+++ _scripts/import_wwc_sunlight_status.py
@@ -7,8 +7,8 @@ def run_through_data(dataframe):
 
     for filename in os.listdir('../_places/'):
         if filename[-3:] == '.md':  # if it's really one of the place files
-            with open('../_places/' + filename, 'w') as my_file:
-                place_pretty, place_id = get_place_names(my_file)
+            with open('../_places/' + filename, 'a') as my_file:
+                place_pretty, place_id = get_place_names(my_file.read())
                 place_id = my_file
             if place_pretty in data['CityAndState'].values:
                 mark(filename, 'wwc', 'true')
@@ -59,13 +59,14 @@ def mark(filename, sunlight_or_wwc, boolean_string):
             filename + ')')
 
     with open(folder + filename, 'w') as my_file:
-        search = re.search(r'\n' + sunlight_or_wwc + r': ', my_file)
+        search = re.search(r'\n' + sunlight_or_wwc + r': ', my_file.read())
         if search:
             print("Property '" + sunlight_or_wwc + "' found for " +
                 filename + ". Value left as-is.")
         else:
             re.sub(next_text,
-                '\n' + sunlight_or_wwc + ': ' + boolean_string + '\n')
+                '\n' + sunlight_or_wwc + ': ' + boolean_string + '\n',
+                my_file.read())
 
     return
 
