a95ed848bad8d0d9b6ef6774425d757b54bb10c2
Greg Jordan-Detamore <greg@gregjd.com>
2017-09-26 17:58:41 -0400

    Make line-level tagging work

--- _scripts/tag_policies_from_spreadsheet.py
+++ _scripts/tag_policies_from_spreadsheet.py
@@ -1,4 +1,5 @@
 import difflib
+import gfm
 import pandas as pd
 import re
 import regex
@@ -75,28 +76,94 @@ def get_guideline_num(title):
 # need definitions for Data, Public Data/Information, Open Data
 
 
+# r'(<.+>)*_____(<.+>)*'
+
+# html = gfm.markdown(content)
+
+
+
+# NEW!!!!!!!
+# for line in file:
+#     get_matches(orig_text, compare_text)
+
 
 
 # MD -> HTML
+# For each element:
+# for each p, heading, li:
+#     look for matches
+
 # Go from beginning to end, stripping out HTML, and adding those tags to a dict that knows the beginning character position
 # Compare strings and get matches
 
 # deal with 'Does the opposite' and 'close fit'
 
-def get_matches(original_text, compare_text, printout=False):
+
+
+# def do_an_entire_doc__all_questions(___):
+
+#     # with file open
+
+#     text_list = ___.split('\n')
+#     # do stuff
+
+
+#     return
+
+def tag_items(orig_text_list, compare_text, tag):
+    """Go through a doc looking for matches for a single definition."""
+
+    new_text_list = orig_text_list[:]
+    for x in range(len(new_text_list)):
+        line_matches = get_matches(new_text_list[x], compare_text)
+        new_text_list[x] = tag_all_matches(new_text_list[x], line_matches, tag)
+        # for match in line_matches:
+        #     line_w_end_tag = insert_html(</, line, match[1])
+        #     line_w_both_tags = insert_html(, match[0])
+        # line = line_w_both_tags
+
+    return new_text_list
+
+def tag_all_matches(text_line, matches, tag):
+
+    new_text_line = text_line
+    # list sorted in reverse order so that HTML is inserted from right to left,
+    # so that insertion doesn't mess up character position numbers
+    for match in sorted(matches, reverse=True):
+        new_text_line = tag_single_match(new_text_line, match, tag)
+
+    return new_text_line
+
+def tag_single_match(text_line, match, tag):
+
+    with_one_tag = insert_html('</span>', text_line, match[1])
+    with_both_tags = insert_html('<span class="' + tag + '">',
+        with_one_tag, match[0])
+
+    return with_both_tags
+
+def insert_html(insertion, string, position):
+
+    return string[:position] + insertion + string[position:]
+
+def get_matches(orig_text, compare_text, printout=False):
     """Takes two texts and returns the char ranges of matching sections.
     Uses simplified matches as described in functions below.
     Prints out the matching text if printout is True."""
 
-    sm = difflib.SequenceMatcher(None, a=original_text,
+    sm = difflib.SequenceMatcher(None, a=orig_text,
         b=compare_text, autojunk=False)
-    output = simplify_match_list(sm.get_matching_blocks(), original_text)
+    output = simplify_match_list(sm.get_matching_blocks(), orig_text)
+    # ranges = simplify_match_list(sm.get_matching_blocks(), orig_text)
+    # output = [orig_text[r[0]:r[1]] for r in ranges if (r[1]-r[0] > 20)]
 
     if printout:
+        # for o in output:
+        #     print(o + '\n\n')
         for match_range in output:
-            print original_text[match_range[0]:match_range[1]] + '\n\n'
+            print orig_text[match_range[0]:match_range[1]] + '\n\n'
 
-    return
+    return output
 
 def simplify_match_list(matching_blocks, source):
     """Takes a list of Match objects and simplifies it to a more consise list
@@ -105,10 +172,13 @@ def simplify_match_list(matching_blocks, source):
     match_positions = get_match_positions(matching_blocks)
     simple_list = reduce(simplify_sequence, match_positions, [(0,0)])
     simple_list_stripped = [strip_left_junk(a, b, source) for (a, b) in simple_list]
-    if (0,0) in simple_list_stripped:
-        simple_list_stripped.remove((0,0),)
+    # if (0,0) in simple_list_stripped:
+    #     simple_list_stripped.remove((0,0),)
+    # for i in simple_list_stripped:
+    #     if i[0] == i[1]
 
-    return simple_list_stripped
+    # return simple_list_stripped
+    return [i for i in simple_list_stripped if (i[1] - i[0] > 25)]
 
 def simplify_sequence(x, y):
     """Groups pairs of character positions if they're close together.
@@ -128,16 +198,35 @@ def simplify_sequence(x, y):
 def get_match_positions(matching_blocks):
     """Converts Match objects to their character ranges in the original text."""
 
-    return [(i[0], i[0]+i[2]) for i in matching_blocks]
+    return [(i[0], i[0]+i[2]) for i in matching_blocks if (i[2] > 5)]
 
 def strip_left_junk(start_pos, end_pos, source):
     """Tells how many characters to offset the beginning of a string that
-    starts with junk characters that we don't want to include."""
+    starts with junk characters that we don't want to include.
+    Some of these are literal junk, while others will mess with
+    Markdown formatting."""
 
     text = source[start_pos:end_pos]
-    strip_length = len(text) - len(text.lstrip('.,; '))
+    # strip_length = len(text) - len(text.lstrip('.,; '))
+    # print 'text'
+    # print text
+    stripped = re.sub(r'^ {0,8}[-.,;*\w\d]{0,2}[-.,;*]\d? {0,4}', '', text,
+        count=1, flags=re.M)
+    # print 'stripped'
+    # print stripped
+    # re.compile(r'\n {0,8}[-.*,;\w\d]{0,2}[-.*,;]\d? {0,4}', re.M)
+    strip_length = len(text) - len(stripped)
+    new_start_pos = start_pos + strip_length
+    # Don't mess up bold beginnings in Markdown:
+    if source[new_start_pos-2:new_start_pos] == '**':
+        new_start_pos -= 2
+    elif source[new_start_pos-1:new_start_pos] == '*':
+        new_start_pos -= 1
 
-    return (start_pos + strip_length, end_pos)
+    # add one or two asterisks back in
+
+    # return (start_pos + strip_length, end_pos)
+    return (new_start_pos, end_pos)
 
 
 # reduce(lambda x, y: if y[0] - x[-1][1] , seq, [(0,0)])
